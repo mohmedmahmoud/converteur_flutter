b6e0d4d651e976ab58da3ea37345899320a564bb
mohmedmahmoud <bouhmed1996@gmail.com>
2020-08-12 20:15:31 +0200

    j ai une problem inconnue

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import "package:flutter/material.dart";
 
-main()=>runApp(converteur());
+main() => runApp(home());
 
+class home extends StatelessWidget {
+  const home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: converteur(),
+      ),
+    );
+  }
+}
 
 class converteur extends StatefulWidget {
   converteur({Key key}) : super(key: key);
@@ -11,57 +23,108 @@ class converteur extends StatefulWidget {
 }
 
 class _converteurState extends State<converteur> {
+  double ougiya = 0.0, dollar = 0.0;
+  bool isDollar = true;
+  var dController = TextEditingController();
+  var oController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    dController.addListener(FOugiya());
+    oController.addListener(FDollar());
+  }
+
+  FDollar() {
+    setState(() {
+      dollar = double.parse(dController.text);
+      ougiya = Convert(dollar, true);
+      if (isDollar) oController.text = ougiya.toString();
+      isDollar = true;
+    });
+  }
+
+  FOugiya() {
+    ougiya = double.parse(oController.text);
+    dollar = Convert(ougiya, false);
+    if (!isDollar) dController.text = dollar.toString();
+    isDollar = false;
+  }
+
+  double Convert(double variable, bool isDol) {
+    return isDol ? (variable / 370.56) : (variable * 370.56);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-       home: Scaffold(
-         appBar: AppBar(
-           title:Text("Converteur",),
-           centerTitle: true,
-         ),
-         body: Row(
-           crossAxisAlignment: CrossAxisAlignment.center,
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: <Widget>[
-            
-           Expanded(
-       
-                        child: Column(
-               children: <Widget>[
-                 Text("OUGIYA"),
-                 TextField(
-                   decoration:InputDecoration(
-                      border: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Converteur",
         ),
-
-                   ),
-                 )
-               ],
-             ),
-           ),
+        centerTitle: true,
+      ),
+      body: Column(children: <Widget>[
+        SizedBox(
+          height: 100,
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "OUGIYA",
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 30,
+                    ),
+                  ),
+                  TextField(
+                    controller: oController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      suffixText: "UM",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
             SizedBox(
-               width: 20,
-             ),
-             Expanded(
-           
-                        child: Column(
-               children: <Widget>[
-                 Text("DOLLAR"),
-                 TextField(
-                   decoration:InputDecoration(
+              width: 20,
+            ),
+            Expanded(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "DOLLAR",
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 30,
+                    ),
+                  ),
+                  TextField(
+                    controller: dController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      suffixIcon: Icon(Icons.attach_money),
                       border: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
-
-                   ),
-                 )
-               ],
-             ),
-           ),
-           ],
-         ),
-       ),
+      ]),
     );
   }
 }
